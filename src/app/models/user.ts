3be930ec6206e19e181e2e9b8b01c48c0494b055
Dania <danilo.anatolievich@gmail.com>
2021-12-24 01:24:33 +0200

    remove sex, update sign up

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,11 +1,8 @@
-import { Sex } from "./sex";
-
 export interface User {
     id: number;
     name: string;
     surname: string;
     age: number;
-    sex: Sex;
     email: string;
     password: string;
 }
