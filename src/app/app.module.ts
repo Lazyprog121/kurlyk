fb08af58d5b5126c7a370e19a612d1884578710c
Dania <danilo.anatolievich@gmail.com>
2021-12-23 22:53:50 +0200

    spacing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,9 +15,9 @@ import { ChatComponent } from './chats/chat/chat.component';
 import { ProfileComponent } from './profile/profile.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { SignInComponent } from './sign-in/sign-in.component';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 
 @NgModule({
