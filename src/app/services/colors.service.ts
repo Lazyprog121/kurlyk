53fd4216272219b2ce9f0929a8d6bceb7f388ca6
Dania <danilo.anatolievich@gmail.com>
2021-12-24 22:58:38 +0200

    fix colors service

--- src/app/services/colors.service.ts
+++ src/app/services/colors.service.ts
@@ -1,18 +1,30 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { AppConstants } from '../app.constants';
+import { AuthorizationService } from './authorization.service';
+import { DataService } from './data.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ColorsService {
   colorChanged$: Subject<string> = new Subject<string>();
-  constructor(private constants: AppConstants) { }
+
+  constructor(private constants: AppConstants,
+    private dataService: DataService,
+    private authorizationService: AuthorizationService) { }
 
   getColorsForCurrentUser() {
-    // TODO: get colors by user's type
+    const userId = this.authorizationService.getAuthorizedUserId() as number;
+    const age = this.dataService.getUserAge(userId) as number;
 
-    return [this.constants.childFirst, this.constants.childSecond];
+    if (age < 18) {
+      return [this.constants.childFirst, this.constants.childSecond];
+    } else if (age < 45) {
+      return [this.constants.youngFirst, this.constants.youngSecond];
+    } else {
+      return [this.constants.oldFirst, this.constants.oldSecond];
+    }
   }
 
   setColor(color: string): void {
