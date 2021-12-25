c5cfa30f294f4a38328f3d8d05cfc348f8d54d00
Dania <danilo.anatolievich@gmail.com>
2021-12-25 03:07:04 +0200

    update contacts

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthorizationService } from '../services/authorization.service';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-contacts',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contacts.component.scss']
 })
 export class ContactsComponent implements OnInit {
+  public contacts: any;
 
-  constructor() { }
+  constructor(
+    private dataService: DataService, 
+    private router: Router,
+    private authorizationService: AuthorizationService
+    ) { }
 
   ngOnInit(): void {
+    const userId = this.authorizationService.getAuthorizedUserId() as number;
+    const usersContacts = this.dataService.getUsersWithoutOne(userId);
+
+    let chatIdCounter = 0;
+    this.contacts = usersContacts.map(x => ({
+      name: x.name + ' ' + x.surname,
+      chatId: chatIdCounter++
+    }));
   }
 
+  openChat(chatId: number) {
+    this.router.navigate(['/chats', chatId]);
+  }
 }
